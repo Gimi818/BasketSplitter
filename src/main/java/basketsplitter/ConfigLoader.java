ea62d4f317b7c550fb7cabfdde202ad5796ecb49
Gimi818 <gimi8183@gmail.com>
2024-04-07 19:26:45 +0200

    Created ConfigLoader class and implementation of loadConfig method

---
+++ src/main/java/basketsplitter/ConfigLoader.java
@@ -0,0 +1,27 @@
+package basketsplitter;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import lombok.extern.log4j.Log4j2;
+@Log4j2
+public class ConfigLoader {
+
+    public static Map<String, List<String>> loadConfig(String filePath) {
+        try {
+            byte[] jsonData = Files.readAllBytes(Paths.get(filePath));
+            ObjectMapper objectMapper = new ObjectMapper();
+                log.info("Successfully loaded configuration from file: {}", filePath);
+            return objectMapper.readValue(jsonData, new TypeReference<Map<String, List<String>>>(){});
+        } catch (IOException e) {
+                log.error("Error loading configuration from file: {}", filePath, e);
+            return null;
+        }
+    }
+}
+
